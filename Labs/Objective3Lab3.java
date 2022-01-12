d52c98bfb5da2a7e23fe4d45f04821874cae7d8b
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-01-12 12:40:36 -0600

    complete objective3lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,11 @@
+class Objective3Lab3{
+  public static void main(String[] args){
+    int first, second, third;
+    first = (int)(Math.random() * 6);
+    second = (int)(Math.random() * 6);
+    third = (int)(Math.random() * 6);
+    int res = first + second + third;
+
+    System.out.println(first + " + " + second + " + " + third + " =  " + res);
+  }
+}
