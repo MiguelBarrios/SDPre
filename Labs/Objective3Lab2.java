466f7f5fc2d2e7953283845f054e9143642b3839
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-01-12 12:35:18 -0600

    complete objective3lab2

--- Labs/Objective3Lab2.java
+++ Labs/Objective3Lab2.java
@@ -5,7 +5,7 @@ class Objective3Lab2 {
     side2 = 8.0;
 
     hypotenuse = Math.sqrt(side1*side1 + side2*side2);
-    System.out.println(hypotenuse);
+    System.out.println("The hypotenuse of a triangle with sides " + side1 + " and " + side2 + " is " + hypotenuse);
 
   }
 }
